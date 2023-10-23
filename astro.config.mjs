b787653d4c6d572cb22ccf74437313d0ca44b639
Johan Torfs <j.t.johan.torfs@gmail.com>
2023-10-23 19:10:19 +0200

    added storyblok config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import vercel from "@astrojs/vercel/serverless";
 
 import svelte from "@astrojs/svelte";
 
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+const env = loadEnv("", process.cwd(), ['STORYBLOK', 'PUBLIC_VERCEL_ENV']);
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
-  integrations: [svelte()]
+  integrations: [
+    storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    }),
+    svelte()
+]
 });
